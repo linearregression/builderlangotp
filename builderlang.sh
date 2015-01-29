fcfb7a061f3fac7d8ea8ee617be3d30399543a2d
root <edward>
2015-01-28 16:42:02 -0800

    disable wx

--- builderlang.sh
+++ builderlang.sh
@@ -2,4 +2,4 @@ export CACHESIZE=`cat /sys/devices/system/cpu/cpu0/cache/index0/coherency_line_s
 echo $CACHESIZE
 
 
-./configure --prefix=/opt/erlang1704 --enable-threads--enable-smp-support --enable-kernel-poll --enable-fp-exceptions --enable-m64-build --with-termcap --without-javac --with-ssh --with-libatomic_ops --enable-dirty-schedulers
+./configure --prefix=/opt/erlang1704 --enable-threads--enable-smp-support --enable-kernel-poll --enable-fp-exceptions --enable-m64-build --with-termcap --without-javac --with-ssh --with-libatomic_ops --enable-dirty-schedulers --without-wx
